0263b963bd0735e0b2d752e491d5d1b5759d2898
Mesut Öztürk <mesutoztrk@msn.com>
2018-12-11 14:27:33 +0300

    mdi container yapısı.

---
+++ CRMEkranYonetimi/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRMEkranYonetimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<int> Sayilar { get; set; }
+        private FrmNew _frmNew;
+        private FrmOpen _frmOpen;
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_frmNew == null || _frmNew.IsDisposed)
+            {
+                _frmNew = new FrmNew();
+                _frmNew.MdiParent = this;
+                _frmNew.Show();
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_frmOpen == null || _frmOpen.IsDisposed)
+            {
+                _frmOpen = new FrmOpen();
+                _frmOpen.MdiParent = this;
+                _frmOpen.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Sayilar = new List<int>() { 1, 2, 3, 4, 2, 1, 2, 3, 4, 5, 3, 2 };
+        }
+    }
+}
---
+++ CRMEkranYonetimi/FrmOpen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRMEkranYonetimi
+{
+    public partial class FrmOpen : Form
+    {
+        public FrmOpen()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmOpen_Load(object sender, EventArgs e)
+        {
+            var sayilar = (this.MdiParent as Form1).Sayilar;
+            sayilar.RemoveAt(3);
+        }
+    }
+}
